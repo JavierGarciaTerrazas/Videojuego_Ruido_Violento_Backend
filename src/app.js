66aa9aaa234cd8fbc7f6996fc13d9943552fef27
Ulifly <ulifly@hotmail.com>
2026-04-21 16:25:14 -0600

    feat(auth): add browser demo and enable CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import { auth } from '../src/middlewares/authMiddleware.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use('/auth', authRoutes);
 
