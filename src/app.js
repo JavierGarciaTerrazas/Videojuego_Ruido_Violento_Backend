2a011526d6ec16867331750a6a85bc1822b854e2
Ulifly <ulifly@hotmail.com>
2026-04-22 18:23:54 -0600

    added roleMiddleware, for  role manage, and the admin route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import { auth } from '../src/middlewares/authMiddleware.js';
+import { authorize } from '../src/middlewares/roleMiddleware.js';
+
 import cors from 'cors';
 
 const app = express();
@@ -16,6 +18,14 @@ app.get('/protegida',auth, (req, res) => {
     user: req.user 
   });
 });
+
+app.get('/admin', auth, authorize('admin', 'super_admin'), (req, res)=> {
+  res.json({
+    message: 'Bienvenido admin 👑',
+    user: req.user,
+    role: req.user.role
+  })
+})
 
 
 
