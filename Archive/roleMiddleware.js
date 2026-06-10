e7d8f712e2cb9f2b8a10a75661b1dad07f6f2fdb
Javier Garcia Terrazas <javi@MacBook-Air-de-Javi.local>
2026-06-09 21:50:25 -0600

    Eliminar Archive del repo, pero conserva local

--- Archive/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-export const authorize = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.role)) {
-            return res.status(403).json({ message: '⛔ Acceso denegado' }); //si el rol del usuario no está incluido en los roles permitidos, se devuelve un error 403 (Forbidden)  
-    }
-        next(); //si el rol del usuario está incluido en los roles permitidos, se llama a next() para continuar con la ejecución de la ruta protegida
-    }
-}
